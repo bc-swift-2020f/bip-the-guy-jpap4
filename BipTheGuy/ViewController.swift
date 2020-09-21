aef4befdba7cee116ba1dd5a8a50bc1468240275
John Pappas <johncpappas4@gmail.com>
2020-09-21 12:13:26 -0400

    Initial Commit

--- BipTheGuy/ViewController.swift
+++ BipTheGuy/ViewController.swift
@@ -9,12 +9,16 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    @IBOutlet weak var imageView: UIImageView!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func punchButtonPressed(_ sender: UIButton) {
+        
+    }
 }
 
